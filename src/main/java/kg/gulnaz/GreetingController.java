1c9115dd9c187bfd845d907e6b5bd992cce9be96
USER <agivetovag@gmail.com>
2019-08-27 15:47:52 +0600

    Task web

---
+++ src/main/java/kg/gulnaz/GreetingController.java
@@ -0,0 +1,14 @@
+package kg.gulnaz;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
